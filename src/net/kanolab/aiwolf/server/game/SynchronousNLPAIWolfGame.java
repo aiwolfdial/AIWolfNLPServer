0e159f8a9637a49a1aac734b8d058b1c9794f267
navleorange <navleorange.0718@gmail.com>
2024-03-09 09:06:22 +0900

    add: check done combinations

--- src/net/kanolab/aiwolf/server/game/SynchronousNLPAIWolfGame.java
+++ src/net/kanolab/aiwolf/server/game/SynchronousNLPAIWolfGame.java
@@ -11,10 +11,14 @@ import org.aiwolf.server.GameData;
 import org.aiwolf.server.net.GameServer;
 
 // 役職の割り振りを保存するためのインポート
-import java.util.TreeSet;
 import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.TreeSet;
+import java.util.ArrayList;
+import java.util.Collections;
 import net.kanolab.aiwolf.server.common.Option;
 import net.kanolab.aiwolf.server.common.GameConfiguration;
 
@@ -29,7 +33,78 @@ public class SynchronousNLPAIWolfGame extends AIWolfGame{
 	}
 
 	/**
+	 * 役職の組み合わせをログに残すか判断する
+	 * 
+	 * @author nwatanabe
+	 */
+	private boolean isWriteRoleCombinations(GameConfiguration config){
+		return config.getBoolean(Option.IS_SAVE_ROLE_COMBINATIONS) && (Integer)config.get(Option.ALL_PARTICIPANT_NUM) > (Integer)config.get(Option.BATTLE_AGENT_NUM);
+	}
+
+	/**
+	 * 現在の役職の組み合わせから1行のテキストを作成する
+	 * 
+	 * @author nwatanabe
+	 */
+	private String makeRoleCombinationsText(){
+		boolean includeAgentNum = false;
+		ArrayList<String> combinationText = new ArrayList<>();
+
+		for(Agent agent:new TreeSet<Agent>(gameData.getAgentList())){
+
+			if(includeAgentNum){
+				combinationText.add(String.format("%d,%s,%s", agent.getAgentIdx(),gameData.getRole(agent), agentNameMap.get(agent)));
+			}
+			else{
+				combinationText.add(String.format("%s,%s",gameData.getRole(agent), agentNameMap.get(agent)));
+			}
+		}
+
+		if(!includeAgentNum){
+			Collections.sort(combinationText);
+		}
+
+		return String.join(",",combinationText);
+	}
+
+	/**
+	 * 今までの組み合わせを保存しているログに今回のログがあるか確認する
+	 * 
+	 * @author nwatanabe
+	 */
+	private boolean isDoneCombinations(GameConfiguration config, String checkCombinationText){
+		File file = new File((String)config.get(Option.ROLE_COMBINATIONS_SAVE_PATH) + (String)config.get(Option.ROLE_COMBINATIONS_SAVE_FILE_NAME));
+		
+		if(!file.exists()){
+			return false;
+		}
+
+		try{
+			FileReader fileReader = new FileReader(file);
+			BufferedReader bufferReader = new BufferedReader(fileReader);
+			String doneCombinationText = "";
+
+			while((doneCombinationText = bufferReader.readLine()) != null){
+				if(doneCombinationText.equals(checkCombinationText)){
+					bufferReader.close();
+					return true;
+				}
+			}
+
+			bufferReader.close();
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+
+		
+		return false;
+	}
+
+	/**
 	 * 毎ゲーム開始時の初期化
+	 * 
+	 * ・追記 2024/03/09 nwatanabe
+	 * 	isWriteRoleCombinations = true なら重複が無いか確認する
 	 */
 	protected void init(){
 		gameDataMap = new TreeMap<Integer, GameData>();
@@ -44,19 +119,33 @@ public class SynchronousNLPAIWolfGame extends AIWolfGame{
 			String requestName = gameServer.requestName(agent);
 			agentNameMap.put(agent, requestName);
 		}
+
+		// check same pattern exist or not
+		GameConfiguration config = new GameConfiguration(DEFAULT_INI_PATH);
+		if(isWriteRoleCombinations(config)){
+			String currentText = makeRoleCombinationsText();
+
+			if(isDoneCombinations(config,currentText)){
+				super.finish();
+			}
+
+		}
+
 	}
 
+	/**
+	 * 今回の役職の組み合わせを保存する
+	 * 
+	 * @author nwatanabe
+	 */
 	private void saveRoleCombinations(){
 		GameConfiguration config = new GameConfiguration(DEFAULT_INI_PATH);
 
-		if(!config.getBoolean(Option.IS_SAVE_ROLE_COMBINATIONS) || (Integer)config.get(Option.ALL_PARTICIPANT_NUM) <= (Integer)config.get(Option.BATTLE_AGENT_NUM)){
+		if(!isWriteRoleCombinations(config)){
 			return;
 		}
 
-		String saveText = "";
-		for(Agent agent:new TreeSet<Agent>(gameData.getAgentList())){
-			saveText += String.format("%d,%s,%s", agent.getAgentIdx(),gameData.getRole(agent), agentNameMap.get(agent));
-		}
+		String saveText = makeRoleCombinationsText() + "\r\n";
 
 		try{
 			File file = new File((String)config.get(Option.ROLE_COMBINATIONS_SAVE_PATH) + (String)config.get(Option.ROLE_COMBINATIONS_SAVE_FILE_NAME));
