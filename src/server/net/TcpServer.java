e4ffbf7077c2ad399189189dc07e60e23a25c7ab
nharu-0630 <8305330+nharu-0630@users.noreply.github.com>
2024-07-21 16:49:15 +0900

    refactor: 不要なクラスを削除

--- src/server/net/TcpServer.java
+++ src/server/net/TcpServer.java
@@ -16,11 +16,8 @@ import java.net.SocketTimeoutException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -31,6 +28,7 @@ import java.util.concurrent.TimeoutException;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
+import common.BRCallable;
 import common.data.Agent;
 import common.data.Request;
 import common.data.Role;
@@ -43,72 +41,29 @@ import server.GameData;
 import server.exception.IllegalPlayerNumException;
 import server.exception.LostClientException;
 
-/**
- * Game server which communicates with clients via Tcp/IP connections.
- *
- * @author tori and otsuki
- *
- */
 public class TcpServer implements GameServer {
 	private static final Logger logger = LogManager.getLogger(TcpServer.class);
 
-	/**
-	 * Server Port
-	 */
 	protected int port;
-
-	/**
-	 * connection limit
-	 */
 	protected int limit;
 
-	/**
-	 *
-	 */
 	protected boolean isWaitForClient;
 
-	/**
-	 *
-	 */
 	protected BidiMap<Socket, Agent> socketAgentMap;
 
-	/**
-	 * Current game data
-	 */
 	protected GameData gameData;
 
-	/**
-	 * Game Setting
-	 */
 	protected GameSetting gameSetting;
 
-	/**
-	 *
-	 */
-	// Logger serverLogger;
-
-	/**
-	 *
-	 */
 	protected Map<Agent, String> nameMap;
-
-	protected Set<ServerListener> serverListenerSet;
 
 	protected Map<Agent, Integer> lastTalkIdxMap;
 	protected Map<Agent, Integer> lastWhisperIdxMap;
 
 	protected ServerSocket serverSocket;
 
-	/**
-	 * Time limit for waiting request
-	 */
 	protected int responseTimeout = 1000;
 
-	/**
-	 *
-	 * @param port
-	 * @param limit
-	 */
 	public TcpServer(int port, int limit, GameSetting gameSetting) {
 		this.gameSetting = gameSetting;
 		this.port = port;
@@ -118,33 +73,12 @@ public class TcpServer implements GameServer {
 		}
 
 		socketAgentMap = new BidiMap<>();
-		// serverLogger = Logger.getLogger(loggerName);
 		nameMap = new HashMap<>();
-		serverListenerSet = new HashSet<>();
-		// serverLogger = AiWolfLoggerFactory.getLogger(loggerName);
-		// serverLogger.setLevel(Level.FINER);
-		// try {
-		// serverLogger = AiWolfLoggerFactory.getLogger(loggerName, new
-		// File(loggerName+".log"));
-		// serverLogger.setLevel(Level.FINER);
-		// } catch (IOException e) {
-		// e.printStackTrace();
-		// serverLogger = AiWolfLoggerFactory.getLogger(loggerName);
-		// }
-
 		lastTalkIdxMap = new HashMap<>();
 		lastWhisperIdxMap = new HashMap<>();
 	}
 
-	/**
-	 *
-	 * @throws IOException
-	 * @throws SocketTimeoutException
-	 */
 	public void waitForConnection() throws IOException, SocketTimeoutException {
-		// int timeout_msec = 1000*60*10; // time out in 10 miniutes
-		// serverLogger.info(String.format("Start Server@port %d\n", port));
-
 		for (Socket sock : socketAgentMap.keySet()) {
 			if (sock != null && sock.isConnected()) {
 				sock.close();
@@ -184,36 +118,11 @@ public class TcpServer implements GameServer {
 				String name = requestName(agent);
 				nameMap.put(agent, name);
 
-				for (ServerListener listener : serverListenerSet) {
-					listener.connected(socket, agent, name);
-				}
+				// socket, agent, name をセットに追加していた
 			}
-
 		}
 		isWaitForClient = false;
 		serverSocket.close();
-	}
-
-	/**
-	 *
-	 */
-	public void stopWaitingForConnection() {
-		isWaitForClient = false;
-		try {
-			if (serverSocket != null) {
-				serverSocket.close();
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		for (Socket s : socketAgentMap.keySet()) {
-			try {
-				s.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-		socketAgentMap.clear();
 	}
 
 	@Override
@@ -223,13 +132,7 @@ public class TcpServer implements GameServer {
 		}
 	}
 
-	/**
-	 * send data to client
-	 * 
-	 * @param agent
-	 * @param sendText
-	 * @TODO Whisperの際に毎回GameInfoを毎回送ってしまう問題の解決．必要が無ければGameInfoを送らなくする
-	 */
+	// :TODO Whisperの際に毎回GameInfoを毎回送ってしまう問題の解決．必要が無ければGameInfoを送らなくする
 	protected void send(Agent agent, Request request) {
 		try {
 			String message;
@@ -289,14 +192,6 @@ public class TcpServer implements GameServer {
 		}
 	}
 
-	/**
-	 * delete talks already sent
-	 * 
-	 * @param agent
-	 * @param list
-	 * @param lastIdxMap
-	 * @return
-	 */
 	protected List<TalkToSend> minimize(Agent agent, List<TalkToSend> list, Map<Agent, Integer> lastIdxMap) {
 		int lastIdx = list.size();
 		if (lastIdxMap.containsKey(agent) && list.size() >= lastIdxMap.get(agent)) {
@@ -306,12 +201,6 @@ public class TcpServer implements GameServer {
 		return list;
 	}
 
-	/**
-	 * send data to client
-	 * 
-	 * @param agent
-	 * @param sendText
-	 */
 	protected Object request(Agent agent, Request request) {
 		try {
 			Socket sock = socketAgentMap.getKey(agent);
@@ -320,7 +209,7 @@ public class TcpServer implements GameServer {
 
 			String line = null;
 			ExecutorService pool = Executors.newSingleThreadExecutor();
-			RequestReadCallable task = new RequestReadCallable(br);
+			BRCallable task = new BRCallable(br);
 			try {
 				Future<String> future = pool.submit(task);
 				try {
@@ -333,7 +222,6 @@ public class TcpServer implements GameServer {
 				pool.shutdownNow();
 			}
 
-			// String line = br.readLine();
 			if (!task.isSuccess()) {
 				throw task.getIOException();
 			}
@@ -358,9 +246,10 @@ public class TcpServer implements GameServer {
 			}
 
 		} catch (InterruptedException | ExecutionException | IOException e) {
-			throw new LostClientException("Lost connection with " + agent + "\t" + getName(agent), e, agent);
+			throw new LostClientException("Lost connection with " + agent + "\t" + nameMap.get(agent), e, agent);
 		} catch (TimeoutException e) {
-			throw new LostClientException(String.format("Timeout %s(%s) %s", agent, getName(agent), request), e, agent);
+			throw new LostClientException(String.format("Timeout %s(%s) %s", agent,
+					nameMap.get(agent), request), e, agent);
 		}
 	}
 
@@ -413,31 +302,26 @@ public class TcpServer implements GameServer {
 	@Override
 	public Agent requestVote(Agent agent) {
 		return (Agent) request(agent, Request.VOTE);
-		// return JSON.decode(result);
 	}
 
 	@Override
 	public Agent requestDivineTarget(Agent agent) {
 		return (Agent) request(agent, Request.DIVINE);
-		// return JSON.decode(result);
 	}
 
 	@Override
 	public Agent requestGuardTarget(Agent agent) {
 		return (Agent) request(agent, Request.GUARD);
-		// return JSON.decode(result);
 	}
 
 	@Override
 	public Agent requestAttackTarget(Agent agent) {
 		return (Agent) request(agent, Request.ATTACK);
-		// return JSON.decode(result);
 	}
 
 	@Override
 	public void finish(Agent agent) {
 		send(agent, Request.FINISH);
-		// send(agent, Request.FINISH);
 	}
 
 	@Override
@@ -448,21 +332,6 @@ public class TcpServer implements GameServer {
 	@Override
 	public void setGameSetting(GameSetting gameSetting) {
 		this.gameSetting = gameSetting;
-	}
-
-	/**
-	 * @return isWaitForClient
-	 */
-	public boolean isWaitForClient() {
-		return isWaitForClient;
-	}
-
-	/**
-	 * @param isWaitForClient
-	 *            セットする isWaitForClient
-	 */
-	public void setWaitForClient(boolean isWaitForClient) {
-		this.isWaitForClient = isWaitForClient;
 	}
 
 	@Override
@@ -482,87 +351,5 @@ public class TcpServer implements GameServer {
 			}
 		}
 		socketAgentMap.clear();
-	}
-
-	/**
-	 * @return serverLogger
-	 */
-	// public Logger getServerLogger() {
-	// return serverLogger;
-	// }
-	//
-	// /**
-	// * @param serverLogger セットする serverLogger
-	// */
-	// public void setServerLogger(Logger serverLogger) {
-	// this.serverLogger = serverLogger;
-	// }
-
-	/**
-	 * add server listener
-	 * 
-	 * @param e
-	 * @return
-	 * @see java.util.Set#add(java.lang.Object)
-	 */
-	public boolean addServerListener(ServerListener e) {
-		return serverListenerSet.add(e);
-	}
-
-	/**
-	 * remove server listener
-	 * 
-	 * @param o
-	 * @return
-	 * @see java.util.Set#remove(java.lang.Object)
-	 */
-	public boolean removeServerListener(ServerListener e) {
-		return serverListenerSet.remove(e);
-	}
-
-	public String getName(Agent agent) {
-		return nameMap.get(agent);
-	}
-
-	/**
-	 * @return responseTimeout
-	 */
-	public int getResponseTimeout() {
-		return responseTimeout;
-	}
-
-	/**
-	 * @param responseTimeout
-	 *            セットする timeLimit
-	 */
-	public void setResponseTimeout(int responseTimeout) {
-		this.responseTimeout = responseTimeout;
-	}
-}
-
-class RequestReadCallable implements Callable<String> {
-	private final BufferedReader br;
-	IOException ioException;
-
-	RequestReadCallable(BufferedReader br) {
-		this.br = br;
-	}
-
-	@Override
-	public String call() {
-		try {
-			return br.readLine();
-		} catch (IOException e) {
-			ioException = e;
-			return null;
-		}
-	}
-
-	public boolean isSuccess() {
-		return ioException == null;
-	}
-
-	public IOException getIOException() {
-		return ioException;
 	}
 }
